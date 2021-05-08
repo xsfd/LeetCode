c3c8a35c116709f4bad1bdedbcd6b60e102c20b7
xsfd <xsfd_yf@126.com>
2021-05-08 22:00:40 +0800

    0134

--- 0134.cpp
+++ 0134.cpp
@@ -3,15 +3,25 @@
 //
 
 #include "0134.h"
-#include "util.h"
-int canCompleteCircuit(vector<int>& gas, vector<int>& cost){
-  int result = 0;
-  return result;
+#include <numeric>  // vector 求和
+
+int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+  if (accumulate(gas.begin(), gas.end(), 0) <
+      accumulate(cost.begin(), cost.end(), 0)) {
+    return -1;
+  }
+  vector<int> one_cost(cost.size(),0);
+  for(int i=0;i<one_cost.size();i++){
+    one_cost[i] = gas[i]-cost[i];
+  }
+  return 1;
 
 }
-void Test0134(){
-  vector<int> gas = {1,2,3,4,5};
-  vector<int> cost={3,4,5,1,2};
-  int result = canCompleteCircuit(gas,cost);
-  std::cout<<result<<std::endl;
+
+// -2,-2,-2,3,3
+void Test0134() {
+  vector<int> gas = {1, 2, 3, 4, 5};
+  vector<int> cost = {3, 4, 5, 1, 2};
+  int result = canCompleteCircuit(gas, cost);
+  std::cout << result << std::endl;
 }
